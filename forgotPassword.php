113e38ea6154305ed2b860b2d9cb616acbc3cb7b
Xun Woon <https://github.com/fireun/project.git>
2020-09-03 22:02:07 +0800

    Edit Email

--- forgotPassword.php
+++ forgotPassword.php
@@ -35,8 +35,8 @@ if(isset($_POST['useremail'])){
             $mail->isSMTP();                                            // Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = 'wxtestmail2001@gmail.com';                     // SMTP username
-            $mail->Password   = 'BW75250Y';                               // SMTP password
+            $mail->Username   = '';                     // SMTP username
+            $mail->Password   = '';                               // SMTP password
             $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
